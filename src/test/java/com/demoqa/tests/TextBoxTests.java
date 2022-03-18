12ee43ec1b006505046524ad2e7218308ac2e811
Anton Demyshev <demyshev14@gmail.com>
2022-03-18 22:52:49 +0300

    write a first test

--- src/test/java/com/demoqa/tests/TextBoxTests.java
+++ src/test/java/com/demoqa/tests/TextBoxTests.java
@@ -2,6 +2,7 @@ package com.demoqa.tests;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,7 +17,18 @@ public class TextBoxTests {
         $("#userEmail").setValue("demyshev14@gmail.com");
         $("#gender-radio-1.custom-control-label").click();
         $("#userNumber").setValue("+79857999889");
-        $("#dateOfBirthInput.form-control");
 
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1997");
+        $(byText("14")).click();
+
+//        $(".subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3").setValue("asd");
+//        $("#hobbies-checkbox-1.custom-control-input").click();
+//        $("#uploadPicture").uploadFile("/Users/demyshev/Pictures/1.jpg");
+//        $("#currentAddress").setValue("Moscow");
+//        $(". css-1wa3eu0-placeholder").selectOption("NCR");
     }
 }
+
