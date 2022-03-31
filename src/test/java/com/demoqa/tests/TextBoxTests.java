484d9ad475cf83863add6c6660b865b38635d1ff
Anton Demyshev <demyshev14@gmail.com>
2022-03-31 20:55:47 +0300

    test passed only with browserSize 600x1800

--- src/test/java/com/demoqa/tests/TextBoxTests.java
+++ src/test/java/com/demoqa/tests/TextBoxTests.java
@@ -1,12 +1,24 @@
 package com.demoqa.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        clearBrowserCookies();
+        Configuration.browserSize = "600x1800";
+        Configuration.holdBrowserOpen = false;
+    }
 
     @Test
     void successTest() {
@@ -15,8 +27,8 @@ public class TextBoxTests {
         $("#firstName").setValue("Anton");
         $("#lastName").setValue("Demyshev");
         $("#userEmail").setValue("demyshev14@gmail.com");
-        $("#gender-radio-1.custom-control-label").click();
-        $("#userNumber").setValue("+79857999889");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("9857999889");
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
@@ -24,11 +36,37 @@ public class TextBoxTests {
         $(".react-datepicker__year-select").selectOption("1997");
         $(byText("14")).click();
 
-//        $(".subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3").setValue("asd");
-//        $("#hobbies-checkbox-1.custom-control-input").click();
-//        $("#uploadPicture").uploadFile("/Users/demyshev/Pictures/1.jpg");
-//        $("#currentAddress").setValue("Moscow");
-//        $(". css-1wa3eu0-placeholder").selectOption("NCR");
+        $("#subjectsInput").setValue("English").pressEnter();
+
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-3']").click();
+
+        File file = new File("src/test/resources/1mb.jpeg");
+        $("input[id='uploadPicture']").uploadFile(file);
+
+        $("#currentAddress").setValue("Moscow");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+
+        $("#submit").scrollTo();
+        $("#submit").click();
+
+    }
+
+    void checkTest() {
+        $(byText("Thanks for submitting the form")).should(appear);
+        $(byText("Anton Demyshev")).should(appear);
+        $(byText("demyshev14@gmail.com")).should(appear);
+        $(byText("Male")).should(visible);
+        $(byText("9857999889")).should(appear);
+        $(byText("14 June, 1997")).should(appear);
+        $(byText("English")).should(appear);
+        $(byText("Sports, Music")).should(appear);
+        $(byText("1mb.jpeg")).should(appear);
+        $(byText("Moscow")).should(appear);
+        $(byText("NCR Delhi")).should(appear);
     }
 }
+
+
 
